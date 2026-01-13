47ceb35a14a07aeddd041fd8ebe39f6df4231dda
유하은 <78515302+YHE0663@users.noreply.github.com>
2026-01-13 23:11:46 +0900

    chore: input 컴포넌트 생성 & css 작업

--- src/app/(auth)/login/LoginClient.jsx
+++ src/app/(auth)/login/LoginClient.jsx
@@ -7,6 +7,7 @@ import { useRouter } from "next/navigation";
 
 import styles from "./Auth.module.scss";
 import Loader from "@/components/loader/Loader";
+import Input from "@/components/Input/Input";
 
 const LoginClient = () => {
   const [email, setEmail] = useState("");
@@ -29,7 +30,7 @@ const LoginClient = () => {
 
   return (
     <>
-      <Loader />
+      {isLoading && <Loader />}
       <section className={styles.page}>
         <div className={styles.container}>
           <h1 className={styles.logo}>
@@ -37,7 +38,29 @@ const LoginClient = () => {
           </h1>
 
           <form onSubmit={loginUser} className={styles.form}>
-            Input
+            <Input
+              email
+              icon="letter"
+              id="email"
+              name="email"
+              label="이메일"
+              placeholder="아이디(이메일)"
+              className={styles.control}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <Input
+              password
+              icon="lock"
+              id="password"
+              name="password"
+              label="비밀번호"
+              placeholder="비밀번호"
+              className={styles.control}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <div className={styles.group}>로그인</div>
             <div className={styles.buttonGroup}>버튼</div>
           </form>
